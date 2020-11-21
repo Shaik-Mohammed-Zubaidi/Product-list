c171f9ce0328d2d7204b7d8222f54990ee38dc69
Shaik_Z <kz981786@gmail.com>
2020-11-22 03:31:39 +0530

    finalCommit

--- Backend/myapi.js
+++ Backend/myapi.js
@@ -2,9 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const requestUtils= require('./utils/requestUtils');
-const productSchema= require('dataLoader');
 
 app.use(express.json());
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    brand: {},
+    price:{},
+    discount: Number,
+    stock:{},
+    created_at: Date
+});
 
 mongoose.connect('mongodb://localhost/greenDeck')
     .then(() => console.log("Connected to MongoDB"))
@@ -15,7 +23,7 @@ const Product = mongoose.model('Product', productSchema);
 
 app.post('/api/v1/greenDeck', (req, res) => {
     Product.find(requestUtils.createFilter(req.body))
-        .then((data)=> res.send(data));
+        .then((data)=> res.send(data)).catch(err=> console.log("error occured",err));
 })
 
 app.listen(5000, () => console.log("Listening......"));
